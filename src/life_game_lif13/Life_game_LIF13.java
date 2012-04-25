e1d18c7181d20c96e168520a53ad2727797c68c5
brenon.alexis@gmail.com <brenon.alexis@gmail.com@841ee047-6743-2fb9-de74-371b7ffa0739>
2012-04-25 12:20:26 +0000

    Pattern aléatoire : OK

--- src/life_game_lif13/Life_game_LIF13.java
+++ src/life_game_lif13/Life_game_LIF13.java
@@ -14,7 +14,7 @@ public class Life_game_LIF13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-		Controlleur c = new Controlleur(10, 10, 0.25f, 2);
+		Controlleur c = new Controlleur(10, 10, 1f, 2);
 		c.startApp();
     }
 }
